c660c2c9d9e25d1659e75425d78054bdd23e9f42
MehakSafdar <mehaksafdar0123@gmail.com>
2024-01-29 23:17:40 +0500

    Implemented try-catch

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
  // Function to convert Word document to PDF using ConvertAPI
-    function convertToPDFUsingAPI(file) {
-        
+ async function convertToPDFUsingAPI(file) {
+        try {        
 // API ko import kro with the help of function (incase if I have to use it another time anywhere)
-import ConvertApi from 'convertapi-js'
+const ConvertApi = require('convertapi-js');
 // secret key insert kro (authenticatation ky liye )
-let convertApi = ConvertApi.auth('1tFCVfMy2kl1yq9Y')
+const convertApi = ConvertApi.auth('1tFCVfMy2kl1yq9Y')
 // parameters done 
-let params = convertApi.createParams()
+const params = convertApi.createParams();
 // select file ko parameters mein add kro 
 params.add('File', elFileInput.files[0]);
 // this will perform the conversion of doc to pdf using API 
-let result = await convertApi.convert('docx', 'pdf', params)
+const result = await convertApi.convert('docx', 'pdf', params);
 
             } catch (error) {
-                // for error
-                reject(error);
+                console.error('Error during conversion:', error.message);
             }
+        }
